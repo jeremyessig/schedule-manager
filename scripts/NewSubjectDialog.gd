03f0f2c0762131818c5228075ee50f571b84f0ed
JeremyEssig <jeremy.essig@gmail.com>
2021-11-09 16:52:02 +0100

    Bug patched: Deleting subject from NewSubjectDialog

--- scripts/NewSubjectDialog.gd
+++ scripts/NewSubjectDialog.gd
@@ -13,12 +13,9 @@ onready var add_lesson_line_edit : LineEdit = $Panel/VBoxContainer/Body/VBoxCont
 onready var remove_lesson_option_button : OptionButton = $Panel/VBoxContainer/Body/VBoxContainer/GridContainer/RemoveLessonOptionButton
 onready var define_lesson_subject_option_button : OptionButton = $Panel/VBoxContainer/Body/VBoxContainer/GridContainer/DefineLessonSubjectOptionButton
 
-func _update_GUI() ->void:
-	Global.update_option_button(remove_subject_option_button, Global.subjects_database)
-	Global.update_option_button(remove_lesson_option_button, Global.lessons_database)
-	Global.update_option_button(define_lesson_subject_option_button, Global.subjects_database)
 
 
+##________________________ Methodes de gestion des donees____________
 func _add_subject_to_database(subject:String) ->void:
 	if add_subject_line_edit.text == "":
 		notification.add_color_override("font_color", Color("#a50000"))
@@ -51,29 +48,53 @@ func _add_lesson_to_database(lesson:String, subject:String) ->void:
 	Signals.emit_signal("lesson_added") # to NewLessonDialog by Signals
 	Global.update_option_button(remove_lesson_option_button, Global.lessons_database)
 	print_debug(Global.lessons_database)
-	
-	
 
 
+func _remove_lesson_from_database() ->void:
+	var lesson_title: String = Global.get_item_string(remove_lesson_option_button)
+	Global.lessons_database.erase(lesson_title)
+	for lesson_card in get_tree().get_nodes_in_group("lesson_cards"):
+		if lesson_card.lesson == lesson_title:
+			lesson_card.delete()
+	_update_GUI()
+
+
+func _remove_subject_from_database() ->void:
+	var subject:String = Global.get_item_string(remove_subject_option_button)
+	var i = Global.subjects_database.find(subject)
+	Global.subjects_database.remove(i)
+	var tmp:Dictionary = {}
+	for k in Global.lessons_database:
+		if Global.lessons_database[k][1] != subject:
+			tmp[k] = Global.lessons_database[k]
+	Global.lessons_database = tmp
+	for lesson_card in get_tree().get_nodes_in_group("lesson_cards"):
+		if !Global.lessons_database.has(lesson_card.lesson):
+			lesson_card.delete()
+	_update_GUI()	
+
+
+##_________________________ Gestion de la GUI _________________________
+func _update_GUI() ->void:
+	Global.update_option_button(remove_subject_option_button, Global.subjects_database)
+	Global.update_option_button(remove_lesson_option_button, Global.lessons_database)
+	Global.update_option_button(define_lesson_subject_option_button, Global.subjects_database)
+
+
+
+##_________________________ Methodes connectees ___________________________________
 func _on_CancelButton_pressed() -> void:
 	notification.text = ""
 	hide()
 
 
 func _on_CreateButton_pressed() -> void:
-	var subject : String = add_subject_line_edit.text
-	_add_subject_to_database(subject)
+	_add_subject_to_database(add_subject_line_edit.text)
 	add_subject_line_edit.text = ""
 	
 
 func _on_RemoveSubjectButton_pressed() -> void:
-	var string = Global.get_item_string(remove_subject_option_button)
-	var i = Global.subjects_database.find(string)
-	Global.subjects_database.remove(i)
-	for k in Global.lessons_database:
-		if Global.lessons_database[k][1] == string:
-			Global.lessons_database.erase(k)
-	_update_GUI()
+	_remove_subject_from_database()
 
 
 func _on_CreateLessonButton_pressed() -> void:
@@ -83,9 +104,4 @@ func _on_CreateLessonButton_pressed() -> void:
 
 
 func _on_RemoveLessonButton_pressed() -> void:
-	var string = Global.get_item_string(remove_lesson_option_button)
-	Global.lessons_database.erase(string)
-	for lesson_card in get_tree().get_nodes_in_group("lesson_cards"):
-		if lesson_card.lesson == string:
-			lesson_card.delete()
-	_update_GUI()
+	_remove_lesson_from_database()
